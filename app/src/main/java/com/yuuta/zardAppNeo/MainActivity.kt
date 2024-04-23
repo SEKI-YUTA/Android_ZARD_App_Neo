b5938486fa4acfbc5b927feb177bf61208500393
SEKI-YUTA <zard.since1991@gmail.com>
2024-04-23 23:33:25 +0900

    initial commit

---
+++ app/src/main/java/com/yuuta/zardAppNeo/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.yuuta.zardAppNeo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.yuuta.zardAppNeo.ui.theme.ZARDAppNeoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ZARDAppNeoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        Text(text = "Hello ZARD App")
+                    }
+                }
+            }
+        }
+    }
+}
