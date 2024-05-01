9e2169023b02282dfca1e305d23910be5bb910cc
SEKI-YUTA <zard.since1991@gmail.com>
2024-05-01 20:34:21 +0900

    Add annotation for hilt

--- app/src/main/java/com/yuuta/zardAppNeo/MainActivity.kt
+++ app/src/main/java/com/yuuta/zardAppNeo/MainActivity.kt
@@ -8,13 +8,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.yuuta.zardAppNeo.data.dataSource.ArtistInformationDataSource
 import com.yuuta.zardAppNeo.ui.theme.ZARDAppNeoTheme
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @Inject lateinit var artistInformationDataSource: ArtistInformationDataSource
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
