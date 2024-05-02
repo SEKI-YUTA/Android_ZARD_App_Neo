b1c483b1176836acb443b6affc825ab4853714ad
SEKI-YUTA <zard.since1991@gmail.com>
2024-05-02 12:34:21 +0900

    Remove unused variable

--- app/src/main/java/com/yuuta/zardAppNeo/MainActivity.kt
+++ app/src/main/java/com/yuuta/zardAppNeo/MainActivity.kt
@@ -10,15 +10,11 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.yuuta.zardAppNeo.data.dataSource.ArtistInformationDataSource
 import com.yuuta.zardAppNeo.ui.theme.ZARDAppNeoTheme
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @Inject lateinit var artistInformationDataSource: ArtistInformationDataSource
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
