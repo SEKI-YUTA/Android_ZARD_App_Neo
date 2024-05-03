4b35e30eda0197c5fc68d7f6ca1fcaf7a2ec7b3b
SEKI-YUTA <zard.since1991@gmail.com>
2024-05-03 21:37:31 +0900

    Add ComposeView to fragment_main.xml

--- app/src/main/java/com/yuuta/zardAppNeo/fragments/MainFragment.kt
+++ app/src/main/java/com/yuuta/zardAppNeo/fragments/MainFragment.kt
@@ -4,10 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
 import com.yuuta.zardAppNeo.R
+import com.yuuta.zardAppNeo.ui.theme.ZARDAppNeoTheme
 
 class MainFragment : Fragment() {
+    lateinit var composeView: ComposeView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -17,6 +28,25 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+        val navHostFragment = requireActivity().supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        composeView = view.findViewById(R.id.composeView)
+
+        composeView.setContent {
+            ZARDAppNeoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        Text("HomeFragment")
+                    }
+                }
+            }
+        }
+
+        return view
     }
 }
