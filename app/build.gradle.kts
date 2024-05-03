b4f65f99bf2478e85d4e974b70a97945f7ec475d
SEKI-YUTA <zard.since1991@gmail.com>
2024-05-03 21:53:55 +0900

    Add navigation-safe-args to dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.hilt.android)
     alias(libs.plugins.kotlinx.serialization)
     alias(libs.plugins.ktlint)
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
