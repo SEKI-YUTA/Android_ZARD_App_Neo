272ec67805ae531b49c6f7fd294c78a59322e0a0
SEKI-YUTA <zard.since1991@gmail.com>
2024-05-03 21:24:19 +0900

    commit MainNavHost because I forgot

---
+++ app/src/main/java/com/yuuta/zardAppNeo/ui/MainNavHost.kt
@@ -0,0 +1,25 @@
+package com.yuuta.zardAppNeo.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.yuuta.discdetail.discDetailScreen
+import com.yuuta.disclist.DISC_LIST_ROUTE
+import com.yuuta.disclist.discListScreen
+import com.yuuta.setting.settingRoute
+import com.yuuta.tracklist.trackListRoute
+
+@Composable
+fun MainNavHost(
+    modifier: Modifier = Modifier,
+    startDestination: String = DISC_LIST_ROUTE,
+) {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = startDestination, modifier = modifier) {
+        discListScreen()
+        discDetailScreen()
+        trackListRoute()
+        settingRoute()
+    }
+}
