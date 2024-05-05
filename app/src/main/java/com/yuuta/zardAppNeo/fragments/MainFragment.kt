13cd38920d391a3aaf4918c15f2b02048ebf3b94
SEKI-YUTA <zard.since1991@gmail.com>
2024-05-06 00:02:16 +0900

    Change to use MainNavHost in MainFragment

--- app/src/main/java/com/yuuta/zardAppNeo/fragments/MainFragment.kt
+++ app/src/main/java/com/yuuta/zardAppNeo/fragments/MainFragment.kt
@@ -4,20 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import com.yuuta.zardAppNeo.R
+import com.yuuta.zardAppNeo.ui.MainNavHost
 import com.yuuta.zardAppNeo.ui.theme.ZARDAppNeoTheme
 
 class MainFragment : Fragment() {
@@ -45,16 +40,7 @@ class MainFragment : Fragment() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    Column(modifier = Modifier.fillMaxSize()) {
-                        Text("HomeFragment")
-                        Spacer(modifier = Modifier.size(16.dp))
-                        Button(onClick = {
-                            val direction = MainFragmentDirections.actionMainFragmentToWebviewFragment("https://www.google.com")
-                            navController.navigate(direction)
-                        }) {
-                            Text("Navigate to WebViewFragment")
-                        }
-                    }
+                    MainNavHost()
                 }
             }
         }
