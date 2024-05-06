d7b77a03479ba4eb9b3b053396cc4070e9b2c015
SEKI-YUTA <zard.since1991@gmail.com>
2024-05-06 15:23:30 +0900

    Remove unnecessary code

--- app/src/main/java/com/yuuta/zardAppNeo/MainActivity.kt
+++ app/src/main/java/com/yuuta/zardAppNeo/MainActivity.kt
@@ -2,11 +2,9 @@ package com.yuuta.zardAppNeo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import com.yuuta.zardAppNeo.data.contract.ArtistInformationRepositoryContract
 import com.yuuta.zardAppNeo.di.annotation.FakeArtistInformationRepositoryAnnotation
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -18,10 +16,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        lifecycleScope.launch {
-            val artistInformation = repository.getArtistInformation()
-            println(artistInformation)
-        }
     }
 }
