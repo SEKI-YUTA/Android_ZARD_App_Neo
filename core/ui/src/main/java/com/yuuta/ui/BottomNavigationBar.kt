cb20391c13513abd06fce0a75f185feb5e92a424
SEKI-YUTA <zard.since1991@gmail.com>
2024-05-06 20:35:17 +0900

    Add label

--- core/ui/src/main/java/com/yuuta/ui/BottomNavigationBar.kt
+++ core/ui/src/main/java/com/yuuta/ui/BottomNavigationBar.kt
@@ -3,6 +3,7 @@ package com.yuuta.ui
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
@@ -36,6 +37,9 @@ fun BottomNavigationBar(
                 },
                 icon = {
                     Icon(imageVector = item.icon, contentDescription = "")
+                },
+                label = {
+                    Text(item.text)
                 }
             )
         }
