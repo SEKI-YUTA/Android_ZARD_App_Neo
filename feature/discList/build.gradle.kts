885dc6d3fc3bf8ef3f0181ed2fc8ac2e11dfa1f0
SEKI-YUTA <zard.since1991@gmail.com>
2024-05-06 13:25:20 +0900

    Add common and core:ui module to dependecy

--- feature/discList/build.gradle.kts
+++ feature/discList/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+    implementation(project(":common"))
+    implementation(project(":core:ui"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
