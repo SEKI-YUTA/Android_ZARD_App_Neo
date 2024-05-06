e7dc1c64f119db05fa357f4d38a5b996f2b7488c
SEKI-YUTA <zard.since1991@gmail.com>
2024-05-06 18:47:18 +0900

    Add dependency :core:ui to each feature

--- feature/trackList/build.gradle.kts
+++ feature/trackList/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+    implementation(project(":core:ui"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
