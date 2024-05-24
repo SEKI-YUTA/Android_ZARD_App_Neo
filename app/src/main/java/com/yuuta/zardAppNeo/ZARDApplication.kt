c3fc43cb7c6b3054f88b4cd773087140ca95cf40
SEKI-YUTA <zard.since1991@gmail.com>
2024-05-24 16:21:36 +0900

    Define data used by preference datastore

--- app/src/main/java/com/yuuta/zardAppNeo/ZARDApplication.kt
+++ app/src/main/java/com/yuuta/zardAppNeo/ZARDApplication.kt
@@ -1,7 +1,17 @@
 package com.yuuta.zardAppNeo
 
 import android.app.Application
+import android.content.Context
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import dagger.hilt.android.HiltAndroidApp
+
+internal const val APP_SETTING_PREFERENCE_NAME = "zard_app_preference_key"
+internal val Context.datastore by preferencesDataStore(name = APP_SETTING_PREFERENCE_NAME)
+
+internal object PreferenceKeys {
+    val APP_SETTING = stringPreferencesKey("app_setting")
+}
 
 @HiltAndroidApp
 class ZARDApplication : Application()
