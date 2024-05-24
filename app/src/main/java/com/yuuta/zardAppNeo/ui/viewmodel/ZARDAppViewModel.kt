9f3415a177f41521767def546fba737ae8af7100
SEKI-YUTA <zard.since1991@gmail.com>
2024-05-24 16:45:08 +0900

    Add process read app setting when launch app

--- app/src/main/java/com/yuuta/zardAppNeo/ui/viewmodel/ZARDAppViewModel.kt
+++ app/src/main/java/com/yuuta/zardAppNeo/ui/viewmodel/ZARDAppViewModel.kt
@@ -1,32 +1,60 @@
 package com.yuuta.zardAppNeo.ui.viewmodel
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.yuuta.common.model.AppSetting
 import com.yuuta.common.model.ArtistInformation
 import com.yuuta.common.model.Lce
+import com.yuuta.zardAppNeo.data.repository.AppSettingRepository
 import com.yuuta.zardAppNeo.data.repository.ArtistInformationRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ZARDAppViewModel
-    @Inject
-    constructor(
-        artistInformationRepository: ArtistInformationRepository,
-    ) : ViewModel() {
-        val _viewState = MutableStateFlow(ZARDAppViewState.INITIAL_VALUE)
-        val viewState = _viewState.asStateFlow()
+@Inject
+constructor(
+    artistInformationRepository: ArtistInformationRepository,
+    @ApplicationContext context: Context,
+) : ViewModel() {
+    val _viewState = MutableStateFlow(ZARDAppViewState.INITIAL_VALUE)
+    val viewState = _viewState.asStateFlow()
 
-        init {
-            _viewState.value = ZARDAppViewState(artistInformationRepository.getArtistInformation())
-        }
-
-        data class ZARDAppViewState(
-            val artistInformation: Lce<ArtistInformation>,
-        ) {
-            companion object {
-                val INITIAL_VALUE = ZARDAppViewState(Lce.Loading)
-            }
+    init {
+        viewModelScope.launch {
+            val appSetting = AppSettingRepository().readAppSetting(context).map { Lce.Content(it) }.first()
+            println(appSetting)
+            val artistInformation = artistInformationRepository.getArtistInformation()
+            _viewState.value = ZARDAppViewState(appSetting = appSetting, artistInformation = artistInformation)
         }
     }
+
+    fun updateAppSetting(context: Context, appSetting: AppSetting) {
+        viewModelScope.launch {
+            _viewState.update {
+                it.copy(appSetting = Lce.Content(appSetting))
+            }
+            AppSettingRepository().writeAppSetting(context, appSetting)
+        }
+    }
+
+    data class ZARDAppViewState(
+        val artistInformation: Lce<ArtistInformation>,
+        val appSetting: Lce<AppSetting>,
+    ) {
+        companion object {
+            val INITIAL_VALUE = ZARDAppViewState(
+                artistInformation = Lce.Loading,
+                appSetting = Lce.Loading,
+            )
+        }
+    }
+}
