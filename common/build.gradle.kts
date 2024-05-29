caeec7feb579d4bcb3f53a2bf7af7f580da1cbcf
SEKI-YUTA <zard.since1991@gmail.com>
2024-05-29 21:13:02 +0900

    Apply plugin ktlintSetupPlugin

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     alias(libs.plugins.kotlinx.serialization)
+    id("com.yuuta.zardapp.buildlogic.ktlintSetupPlugin")
 }
 
 android {
--- feature/setting/build.gradle.kts
+++ feature/setting/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.yuuta.zardapp.buildlogic.ktlintSetupPlugin")
 }
 
 android {
