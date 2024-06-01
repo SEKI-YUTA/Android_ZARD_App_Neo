49f9a443db81597d2cab940d8b7344cbd9532161
SEKI-YUTA <zard.since1991@gmail.com>
2024-06-01 13:38:16 +0900

    Add buildFeature and composeOptions section to solve that error  org.jetbrains.kotlin.backend.common.BackendException: Backend Internal error: Exception during IR lowering File being compiled: /Users/sekiyuuta/Desktop/apps/Android_ZARD_App_Neo/resource/src/main/java/com/yuuta/resource/theme/Theme.kt The root cause java.lang.RuntimeException was thrown at: org.jetbrains.kotlin.backend.jvm.codegen.FunctionCodegen.generate(FunctionCodegen.kt:51)

--- resource/build.gradle.kts
+++ resource/build.gradle.kts
@@ -31,10 +31,18 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
 }
 
 dependencies {
-
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.material3)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
