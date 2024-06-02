7591aee32268e13b6f5fb3397c38d30237100582
SEKI-YUTA <zard.since1991@gmail.com>
2024-06-02 10:28:52 +0900

    Create Menu button for Scaffold

---
+++ core/ui/src/main/java/com/yuuta/ui/ScaffoldDrawerMenuButton.kt
@@ -0,0 +1,30 @@
+package com.yuuta.ui
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.rotate
+
+@Composable
+fun ScaffoldMenuButton(animationValue: Float, onClick: () -> Unit) {
+    IconButton(
+        modifier = Modifier.rotate((animationValue * 1.8).toFloat()),
+        onClick = onClick,
+    ) {
+        Icon(
+            modifier = Modifier.alpha(1f - (animationValue / 100f)),
+            imageVector = Icons.Rounded.Menu,
+            contentDescription = "Menu",
+        )
+        Icon(
+            modifier = Modifier.alpha(animationValue / 100f),
+            imageVector = Icons.Rounded.Close,
+            contentDescription = "Close"
+        )
+    }
+}
