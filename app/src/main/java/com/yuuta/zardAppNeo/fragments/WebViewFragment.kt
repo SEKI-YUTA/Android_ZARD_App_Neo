b434dc97dd5d856e3e6bbc68e3e815678f78ac7e
SEKI-YUTA <zard.since1991@gmail.com>
2024-06-10 16:01:13 +0900

    Add WebView to WebViewFragment

--- app/src/main/java/com/yuuta/zardAppNeo/fragments/WebViewFragment.kt
+++ app/src/main/java/com/yuuta/zardAppNeo/fragments/WebViewFragment.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.yuuta.zardAppNeo.R
 
 class WebViewFragment : Fragment() {
     private val args: WebViewFragmentArgs by navArgs()
-
+    private lateinit var webView: WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -23,7 +25,14 @@ class WebViewFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_webview, container, false)
-        view.findViewById<TextView>(R.id.argumentTest).text = args.webUrl
+        webView = view.findViewById(R.id.webView)
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
+        }
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl(args.webUrl)
         return view
     }
 }
