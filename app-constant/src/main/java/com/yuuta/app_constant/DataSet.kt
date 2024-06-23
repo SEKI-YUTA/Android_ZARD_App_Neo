c129e55a4a27e375cfaf46bd7d13f560cf508c83
SEKI-YUTA <zard.since1991@gmail.com>
2024-06-23 10:42:26 +0900

    Add app constant values for composable preview

---
+++ app-constant/src/main/java/com/yuuta/app_constant/DataSet.kt
@@ -0,0 +1,124 @@
+package com.yuuta.app_constant
+
+import com.yuuta.common.model.Disc
+import com.yuuta.common.model.Track
+
+val discList = listOf(
+    Disc(
+        id = 1,
+        name = "Good-bye My Loneliness",
+        releaseYear = "1991",
+        imageName = "index1_1991_02_10_1stsingle.jpg",
+        officialPageURL = "https://www.wezard.net/",
+        discType = "シングル",
+        indexStr = "1st Single",
+        releaseMonth = "2",
+        releaseDate = "10",
+        is8cm = true,
+        trackList =
+        listOf(
+            Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+            Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+            Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+            Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+            Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+        ),
+    ),
+    Disc(
+        id = 2,
+        name = "もう探さない",
+        releaseYear = "1991",
+        imageName = "index5_1991_12_25_2ndalbum.jpg",
+        officialPageURL = "https://www.wezard.net/",
+        discType = "アルバム",
+        indexStr = "2nd Album",
+        releaseMonth = "12",
+        releaseDate = "25",
+        is8cm = false,
+        trackList =
+            listOf(
+                Track("もう探さない", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+                Track("もう探さない", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+                Track("もう探さない", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+                Track("もう探さない", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+                Track("もう探さない", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+            ),
+        ),
+    Disc(
+        id = 3,
+        name = "HOLD ME",
+        releaseYear = "1992",
+        imageName = "index7_1992_09_02_3rdalbum.jpg",
+        officialPageURL = "https://www.wezard.net/",
+        discType = "アルバム",
+        indexStr = "1st Single",
+        releaseMonth = "9",
+        releaseDate = "2",
+        is8cm = false,
+        trackList =
+        listOf(
+            Track("眠れない夜を抱いて", "坂井泉水", "織田哲郎", "葉山たけし", "1992"),
+            Track("眠れない夜を抱いて", "坂井泉水", "織田哲郎", "葉山たけし", "1992"),
+            Track("眠れない夜を抱いて", "坂井泉水", "織田哲郎", "葉山たけし", "1992"),
+            Track("眠れない夜を抱いて", "坂井泉水", "織田哲郎", "葉山たけし", "1992"),
+            Track("眠れない夜を抱いて", "坂井泉水", "織田哲郎", "葉山たけし", "1992"),
+        ),
+    ),
+    Disc(
+        id = 4,
+        name = "IN MY ARMS TONIGHT",
+        releaseYear = "1993",
+        imageName = "index8_1992_09_09_5thsingle.jpg",
+        officialPageURL = "https://www.wezard.net/",
+        discType = "シングル",
+        indexStr = "1st Single",
+        releaseMonth = "9",
+        releaseDate = "9",
+        is8cm = true,
+        trackList =
+        listOf(
+            Track("IN MY ARMS TONIGHT", "坂井泉水", "織田哲郎", "葉山たけし", "1992"),
+        ),
+    ),
+    Disc(
+        id = 5,
+        name = "揺れる想い",
+        releaseYear = "1993",
+        imageName = "index11_1993_05_19_8thsingle.jpg",
+        officialPageURL = "https://www.wezard.net/",
+        discType = "シングル",
+        indexStr = "1st Single",
+        releaseMonth = "5",
+        releaseDate = "19",
+        is8cm = true,
+        trackList =
+        listOf(
+            Track("揺れる想い", "坂井泉水", "織田哲郎", "葉山たけし", "1993"),
+        ),
+    ),
+)
+
+val tooManyTrackList = listOf(
+    Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+    Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+    Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+    Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+    Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+    Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+    Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+    Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+    Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+    Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+    Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+    Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"), Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+    Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+    Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+    Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+    Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+    Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"), Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+    Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+    Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+    Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+    Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+    Track("Good-bye My Loneliness", "坂井泉水", "織田哲郎", "葉山たけし", "1991"),
+)
