fba5ba507bd5f1e6be85e63e788399f817959734
SEKI-YUTA <zard.since1991@gmail.com>
2024-06-24 23:50:50 +0900

    Add capture image test

--- app/src/test/java/com/yuuta/zardAppNeo/CaptureZARDAppUITest.kt
+++ app/src/test/java/com/yuuta/zardAppNeo/CaptureZARDAppUITest.kt
@@ -1,24 +1,26 @@
 package com.yuuta.zardAppNeo
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onRoot
-import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
+import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
 import com.github.takahirom.roborazzi.captureRoboImage
+import com.yuuta.app_constant.discList
+import com.yuuta.app_constant.tooManyTrackList
+import com.yuuta.discdetail.DiscDetailScreen
+import com.yuuta.disclist.DiscListScreen
+import com.yuuta.setting.SettingScreen
+import com.yuuta.tracklist.TrackListScreen
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 import org.robolectric.annotation.GraphicsMode
 
 @GraphicsMode(GraphicsMode.Mode.NATIVE)
 @RunWith(RobolectricTestRunner::class)
+@Config(qualifiers = RobolectricDeviceQualifiers.Pixel7)
 class CaptureZARDAppUITest {
     @get:Rule
     val composeRule = createComposeRule()
@@ -27,15 +29,53 @@ class CaptureZARDAppUITest {
     fun captureDiscListScreen() {
         composeRule
             .setContent {
-                Box(
-                    modifier =
-                        Modifier
-                            .size(310.dp)
-                            .background(Color.Blue),
-                    contentAlignment = Alignment.Center,
-                ) {
-                    Text("Hello Roborazzi")
-                }
+                DiscListScreen(
+                    navController = rememberNavController(),
+                    navigateToDetail = {},
+                    discList = discList,
+                )
+            }
+
+        composeRule.onRoot().captureRoboImage()
+    }
+
+    @Test
+    fun captureDiscDetailScreen() {
+        composeRule
+            .setContent {
+                DiscDetailScreen(
+                    navController = rememberNavController(),
+                    disc = discList.first(),
+                )
+            }
+
+        composeRule.onRoot().captureRoboImage()
+    }
+
+    @Test
+    fun captureTrackListScreen() {
+        composeRule
+            .setContent {
+                TrackListScreen(
+                    navController = rememberNavController(),
+                    openWebUrl = {},
+                    allTrackList = tooManyTrackList,
+                )
+            }
+
+        composeRule.onRoot().captureRoboImage()
+    }
+
+    @Test
+    fun captureSettingScreen() {
+        composeRule
+            .setContent {
+                SettingScreen(
+                    navController = rememberNavController(),
+                    openWebUrl = {},
+                    appSetting = null,
+                    updateAppSetting = { _, _ -> },
+                )
             }
 
         composeRule.onRoot().captureRoboImage()
