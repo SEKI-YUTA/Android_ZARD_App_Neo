34ea81f82525e80bae6b4b5a18272829e8d477f9
SEKI-YUTA <zard.since1991@gmail.com>
2024-07-11 22:15:19 +0900

    Fix Repository Annotation

--- app/src/main/java/com/yuuta/zardAppNeo/MainActivity.kt
+++ app/src/main/java/com/yuuta/zardAppNeo/MainActivity.kt
@@ -3,14 +3,14 @@ package com.yuuta.zardAppNeo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.yuuta.zardAppNeo.data.contract.ArtistInformationRepositoryContract
-import com.yuuta.zardAppNeo.di.annotation.FakeArtistInformationRepositoryAnnotation
+import com.yuuta.zardAppNeo.di.annotation.ArtistInformationRepositoryAnnotation
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     @Inject
-    @FakeArtistInformationRepositoryAnnotation
+    @ArtistInformationRepositoryAnnotation
     lateinit var repository: ArtistInformationRepositoryContract
 
     override fun onCreate(savedInstanceState: Bundle?) {
