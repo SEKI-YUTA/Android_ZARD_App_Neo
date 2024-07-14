1271bdec809244fc5b0db20114943e3045f0187f
SEKI-YUTA <zard.since1991@gmail.com>
2024-07-14 16:28:44 +0900

    Remove Annotations used in DataModule

--- app/src/main/java/com/yuuta/zardAppNeo/MainActivity.kt
+++ app/src/main/java/com/yuuta/zardAppNeo/MainActivity.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.yuuta.zardAppNeo.data.contract.ArtistInformationRepositoryContract
-import com.yuuta.zardAppNeo.di.annotation.ArtistInformationRepositoryAnnotation
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     @Inject
-    @ArtistInformationRepositoryAnnotation
     lateinit var repository: ArtistInformationRepositoryContract
 
     override fun onCreate(savedInstanceState: Bundle?) {
